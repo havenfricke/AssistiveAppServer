8fec849187e477c78047890b8dfba3c9371475fa
havenf <havenfricke@u.boisestate.edu>
2025-05-07 20:18:20 -0600

    net id updates

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.use(express.json());
 const items = [
   {
     id: '7fa2e3a4-2d6f-4c6e-90e0-bfda8e3432d4',
-    netID: 'assistiveapp-cfa',
+    netID: 'aacfa',
     desc: 'BSU Campus Chick-fil-A - Student Union Bldg'
   },
   {
     id: '123e4567-e89b-12d3-a456-426614174000',
-    netID: 'assistiveapp-c32',
+    netID: 'aac32',
     desc: 'BSU Campus Cafe 32 - Albertson\'s Library'
   }
 ];
